08d1d1359569df2116f72bfb9c3a017a0e0df111
Jonh Edson <jonhedson@users.noreply.github.com>
2020-03-25 15:15:28 -0300

    Initial commit

---
+++ app/src/main/java/com/orafaaraujo/sweetdroid/MainActivity.java
@@ -0,0 +1,33 @@
+package com.orafaaraujo.sweetdroid;
+
+import android.content.Intent;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // O reporte de crashs (erros) do aplicativo só estará habilitado para builds apotando para
+        // o ambiente de Testes e ambiente de Produção.
+        // Isso evitará que o aplicativo fique enviando os erros que aconterá enquanto estiver
+        // desenvolvendo.
+        if (BuildConfig.REPORT_CRASH) {
+            // Iniciar serviço de reportar crashs!
+        }
+
+        //noinspection ConstantConditions
+        findViewById(R.id.main_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SecondActivity.class));
+            }
+        });
+    }
+}
